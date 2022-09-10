8fda4812445c4fc35d246b71c3971a167642c061
WizMe-M <timkin.moxim@mail.ru>
2022-09-10 19:12:58 +0300

    вариант 2

--- Test/MyControls/ClickPanel.axaml.cs
+++ Test/MyControls/ClickPanel.axaml.cs
@@ -1,22 +1,36 @@
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
 using Brushes = Avalonia.Media.Brushes;
 
 namespace Test.MyControls
 {
     public class ClickPanel : Button
     {
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+            var panel = e.NameScope.Get<StackPanel>("ButtonPanel");
+            foreach (var logical in panel.GetLogicalChildren())
+            {
+                if (logical is not Button btn) continue;
+                btn.Click += OnButtonClick;
+            } 
+        }
+
         /// <summary>
         /// Обработчик нажатия на кнопки
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void OnButtonClick(object sender)
+        private void OnButtonClick(object? sender, RoutedEventArgs e)
         {
             if (sender is not Button clicked) return;
-            
+
             clicked.Background = Brushes.Aquamarine;
         }
-        
     }
 }
