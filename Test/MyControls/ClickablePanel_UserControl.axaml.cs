1e430de03a110bf066da104d364e1598c3e9bd29
WizMe-M <timkin.moxim@mail.ru>
2022-09-10 19:15:44 +0300

    вариант 3

---
+++ Test/MyControls/ClickablePanel_UserControl.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Test.MyControls;
+
+public partial class ClickablePanel_UserControl : UserControl
+{
+    public ClickablePanel_UserControl()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button clicked) return;
+
+        clicked.Background = Brushes.Aquamarine;
+    }
+}
